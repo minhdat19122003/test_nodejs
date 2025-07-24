13dda7561268361f2e2f05c0ac9871e5844fc39c
minhdat19122003 <phamngominhdat01@gmail.com>
2025-07-23 19:02:02 -0700

    23.07.2025 + Sửa lại thành chữ nigga

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-let mysql = require('mysql2');
-
-let con = mysql.createConnection({
-  host: "172.17.0.2",
-  user: "root",
-  password: "nig",
-  port:3306
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+console.log("Nigga");
